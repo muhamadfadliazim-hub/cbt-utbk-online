c3a35a3b51d41f0572115474dea68eb573be9efe
Muhamad Fadli Azim <muhamadfadliazim@MacBook-Pro-2.local>
2025-12-08 06:51:43 +0700

    Fix API URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,1 @@
-// Jika di Railway (ada env variable), pakai link Railway.
-// Jika di Laptop (tidak ada env), pakai localhost.
-export const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
+export const API_URL = 'https://cbt-utbk-online-production.up.railway.app';
