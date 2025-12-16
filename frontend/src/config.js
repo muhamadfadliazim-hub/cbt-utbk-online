20d7e0fc72896c465cd3c811a1fe0b516a2b4e92
Muhamad Fadli Azim <muhamadfadliazim@192.168.1.9>
2025-12-16 07:36:48 +0700

    Fix: Update API URL to Railway Production

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,7 @@
-export const API_URL = 'https://cbt-utbk-online-production.up.railway.app';
+// frontend/src/config.js
+
+export const API_URL = "https://cbt-utbk-online-production.up.railway.app";
+
+// PERHATIKAN:
+// 1. Wajib pakai "https://" di depan.
+// 2. JANGAN pakai tanda garis miring (/) di paling belakang.
