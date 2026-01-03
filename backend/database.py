7309d9a276e13cedd07bae98d8912ca35cb8f3a5
Muhamad Fadli Azim <muhamadfadliazim@MacBook-Pro-2.local>
2026-01-03 18:43:09 +0700

    Fix: New database version and route check

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,11 @@
+# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# DB v6 = Final Version with Full Features
-SQLALCHEMY_DATABASE_URL = "sqlite:///./utbk_v20_final.db"
+# Ubah nama file .db di sini (misal ke v21)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./eduprime_v21.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
