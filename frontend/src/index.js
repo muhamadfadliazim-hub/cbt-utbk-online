727a1b04de70f654e33bc76e578a5a8b9871b926
Muhamad Fadli Azim <muhamadfadliazim@MacBook-Pro-2.local>
2026-01-03 14:00:26 +0700

    FIX: Lint errors and useEffect dependencies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-// Kita tidak menggunakan BrowserRouter di sini agar kontrol penuh ada di App.js
-// Ini mencegah error routing di server statis Railway
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
